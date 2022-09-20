9ededcbc358419999297437ee8300b1c3901aab4
Manish Trigunayak <manishtrigunayak@gmail.com>
2022-09-20 23:04:07 +0530

    code fixed by gyani

--- prime.js
+++ prime.js
@@ -1,9 +1,10 @@
-let count=0
+
+	let counter=0
     for(i=0;i<=num;i++){
         if (num%i==0)
-        count++
-    }if (count!=2){
+        counter++
+    }if (counter==2){
         console.log("Yes")
     }else {
         console.log("No")
-    }
+	
